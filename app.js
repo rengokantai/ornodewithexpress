19a6c959ac437c482401e7dfaaababca2e47a183
Hernan Y.Ke <yuriqiao@gmail.com>
2016-05-04 08:59:41 -0400

    api.js using API, needs review

--- app.js
+++ app.js
@@ -10,14 +10,17 @@ app.set('view engine', 'jade');
 
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
+app.use(express.static("node_modules/jquery/dist"));
 app.use(bodyParser.urlencoded({ extended:   true }));
-
+app.use(bodyParser.json());
 app.get('/', function (req, res) {
-    res.render("index", { title: "Home"});
+    res.render("home", { title: "Home"});
 });
 
 var adminRouter = require("./admin");
 app.use("/admin", adminRouter);
+var apiRouter = require("./api");
+app.use("/api", apiRouter);
 app.listen(3000, function () {
     console.log('');
 });
