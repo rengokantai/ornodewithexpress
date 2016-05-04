199833e8d11c3b9122b2a99f8ea6444b43b3ded4
Hernan Y.Ke <yuriqiao@gmail.com>
2016-05-03 22:26:11 -0400

    var router=express.Router(); router.route('/').all(function(req,res,next){...next()}).get().post()
    ....
    res.locals.room

--- admin.js
+++ admin.js
@@ -29,30 +29,25 @@ router.route('/rooms/add')
   });
 
 router.route('/rooms/edit/:id')
-  .get(function (req, res) {
-    var roomId = req.params.id;
+    .all(function(req, res, next){
+      var roomId = req.params.id;
 
-    var room = _.find(rooms, r => r.id === roomId);
-    if (!room) {
-      res.sendStatus(404);
-      return;
-    }
+      var room = _.find(rooms, r => r.id === roomId);
+      if (!room) {
+        res.sendStatus(404);
+        return;
+      }
+      res.locals.room = room;
+      next()
+    })
+    .get(function (req, res) {
+      res.render("edit");
+    })
+    .post(function (req, res) {
+      res.locals.room.name = req.body.name;
 
-    res.render("edit", {room});
-  })
-  .post(function (req, res) {
-    var roomId = req.params.id;
-
-    var room = _.find(rooms, r => r.id === roomId);
-    if (!room) {
-      res.sendStatus(404);
-      return;
-    }
-
-    room.name = req.body.name;
-
-    res.redirect(req.baseUrl + "/rooms");
-  });
+      res.redirect(req.baseUrl + "/rooms");
+    });
 
 router.get('/rooms/delete/:id', function (req, res) {
   var roomId = req.params.id;
